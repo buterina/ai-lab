f6c9aaacb82882fcbc3f7e4b93d775d14fbc66c4
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-04-17 20:05:26 +0200

    feat: add breakpoints

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,35 @@
-const swiper = new Swiper('.swiper', {
+const aboutSwiper = new Swiper('.news-swiper', {
 
   direction: 'horizontal',
   loop: true,
   slidesPerView: 3,
-  spaceBetween: 20,
   centeredSlides: true,  // Если true, то активный слайд будет центрирован, а не всегда слева.
   centeredSlidesBounds: true,  // Если true, то активный слайд будет центрирован без добавления пробелов в начале и конце слайдера. Требуется centeredSlides: true.
 
+  breakpoints: {
+    320: {
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+    720: {
+
+      slidesPerGroup: 2,
+      spaceBetween: 10,
+    },
+    1100: {
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+    }
+  },
 
   pagination: {
-    el: '.swiper-pagination',
+    el: '.news-swiper__pagination',
+    clickable: true,
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.news-swiper__button_type_next',
+    prevEl: '.news-swiper__button_type_prev',
   },
-
-
-
 
 });
